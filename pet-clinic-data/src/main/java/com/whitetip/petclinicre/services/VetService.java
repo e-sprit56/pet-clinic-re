5ca8305fb0bd88ec44b0496f3729e669ab95e8b9
e-sprit56 <ksamburski@gmail.com>
2022-11-01 09:48:51 +0100

    Refactor to common crud repository

--- pet-clinic-data/src/main/java/com/whitetip/petclinicre/services/VetService.java
+++ pet-clinic-data/src/main/java/com/whitetip/petclinicre/services/VetService.java
@@ -4,11 +4,6 @@ import com.whitetip.petclinicre.model.Vet;
 
 import java.util.Set;
 
-public interface VetService {
+public interface VetService extends CrudService<Vet, Long> {
 
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
