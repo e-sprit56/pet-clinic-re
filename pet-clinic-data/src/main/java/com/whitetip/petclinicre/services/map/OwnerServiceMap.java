21f95b54ad17148dce337f212477a2d490ff7dcf
e-sprit56 <ksamburski@gmail.com>
2022-11-02 18:25:33 +0100

    Add spring config for services

--- pet-clinic-data/src/main/java/com/whitetip/petclinicre/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/com/whitetip/petclinicre/services/map/OwnerServiceMap.java
@@ -3,9 +3,11 @@ package com.whitetip.petclinicre.services.map;
 import com.whitetip.petclinicre.model.Owner;
 import com.whitetip.petclinicre.services.CrudService;
 import com.whitetip.petclinicre.services.OwnerService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
     @Override
     public Set<Owner> findAll() {
--- pet-clinic-data/src/main/java/com/whitetip/petclinicre/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/whitetip/petclinicre/services/map/VetServiceMap.java
@@ -3,9 +3,11 @@ package com.whitetip.petclinicre.services.map;
 import com.whitetip.petclinicre.model.Vet;
 import com.whitetip.petclinicre.services.CrudService;
 import com.whitetip.petclinicre.services.VetService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
     @Override
     public Set<Vet> findAll() {
