5aad1ff03e9affa71f6a12f1ec8383662db06358
e-sprit56 <ksamburski@gmail.com>
2022-11-02 08:51:52 +0100

    Refactor controllers

--- pet-clinic-web/src/main/java/com/whitetip/petclinicre/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/whitetip/petclinicre/controllers/VetController.java
@@ -2,11 +2,13 @@ package com.whitetip.petclinicre.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/vets")
 public class VetController {
 
-    @GetMapping("/vets")
+    @GetMapping("")
     public String listVets() {
         return "vets/list";
     }
