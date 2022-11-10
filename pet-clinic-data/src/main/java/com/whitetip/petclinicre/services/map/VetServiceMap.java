f8a6728f996310c8ae8553ad8920a2189c8f403d
e-sprit56 <ksamburski@gmail.com>
2022-11-10 06:49:57 +0100

    Load specialities data

--- pet-clinic-data/src/main/java/com/whitetip/petclinicre/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/whitetip/petclinicre/services/map/VetServiceMap.java
@@ -1,7 +1,8 @@
 package com.whitetip.petclinicre.services.map;
 
+import com.whitetip.petclinicre.model.Speciality;
 import com.whitetip.petclinicre.model.Vet;
-import com.whitetip.petclinicre.services.CrudService;
+import com.whitetip.petclinicre.services.SpecialityService;
 import com.whitetip.petclinicre.services.VetService;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +10,13 @@ import java.util.Set;
 
 @Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+
+    private final SpecialityService specialityService;
+
+    public VetServiceMap(SpecialityService specialityService) {
+        this.specialityService = specialityService;
+    }
+
     @Override
     public Set<Vet> findAll() {
         return super.findAll();
@@ -28,6 +36,16 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetS
 
     @Override
     public Vet save(Vet vet) {
+
+        if(vet.getSpecialities().size() > 0){
+            vet.getSpecialities().forEach(speciality -> {
+                if(speciality.getId() == null){
+                    Speciality savedSpeciality = specialityService.save(speciality);
+                    speciality.setId(savedSpeciality.getId());
+
+                }
+            });
+        }
         return super.save(vet);
     }
 
