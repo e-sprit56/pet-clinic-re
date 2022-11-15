e6b4b7a741007873747d17048f75224c8d136cd9
e-sprit56 <ksamburski@gmail.com>
2022-11-15 07:20:13 +0100

    Convert Owner and Pet to entities

--- pet-clinic-data/src/main/java/com/whitetip/petclinicre/model/Owner.java
+++ pet-clinic-data/src/main/java/com/whitetip/petclinicre/model/Owner.java
@@ -1,13 +1,21 @@
 package com.whitetip.petclinicre.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person {
 
+    @Column(name = "address")
     private String address;
+    @Column(name = "city")
     private String city;
+    @Column(name = "telephone_number")
     private String telephoneNumber;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
 
     public String getAddress() {
